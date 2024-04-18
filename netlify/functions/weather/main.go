70cc5425a182673037aae3503fa305e28a5a8dd9
fvrests <fvrests@icloud.com>
2024-04-17 21:50:21 -0500

    add rate limiting

--- netlify/functions/weather/main.go
+++ netlify/functions/weather/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"io"
-	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -20,16 +20,34 @@ var HEADERS = map[string]string{
 	"Vary":                         "Origin",
 }
 
+// Temporarily store request history per IP -- see "rate limiting without overhead"
+// Will be lost on cold start, but will be retained in memory for subsequent requests
+// info: https://lihbr.com/posts/rate-limiting-without-overhead-netlify-or-vercel-functions
+// note: should be tested at deploy link -- memory cache not retained in local environment
+var history = make(map[string]time.Time)
+
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
-	latitude := request.QueryStringParameters["latitude"]
+	// get client IP
+	// info: https://answers.netlify.com/t/include-request-ip-address-on-event-object/1820/5
+	var ip = request.Headers["x-nf-client-connection-ip"] // user ip
 
+	// previous request from this IP is <15 seconds ago - return / block request
+	if history[ip].After(time.Now().Add(-1 * 15 * time.Second)) {
+		return &events.APIGatewayProxyResponse{StatusCode: 429, Body: "Rate limit exceeded"}, nil
+	}
+
+	// previous request >15 seconds ago - allow request & store the time of this request
+	history[ip] = time.Now()
+
+	latitude := request.QueryStringParameters["latitude"]
 	longitude := request.QueryStringParameters["longitude"]
 
 	if latitude == "" || longitude == "" {
-		log.Println("latitude and longitude are required")
+		return &events.APIGatewayProxyResponse{StatusCode: 400, Body: "Missing latitude and/or longitude"}, nil
 	}
 
+	// openweather api request
 	baseUrl := "https://api.openweathermap.org/data/2.5/weather"
 	// local .env for dev environment, netlify env for production
 	appId := os.Getenv("OPENWEATHER_KEY")
@@ -53,4 +71,5 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 
 func main() {
 	lambda.Start(handler)
+
 }
